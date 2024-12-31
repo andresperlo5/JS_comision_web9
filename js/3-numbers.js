ca112c39fe939744e851e2ace0cc5a276391f2a8
unknown <andresperlo5@gmail.com>
2024-12-30 21:32:00 -0300

    clase de hoy condicionales

---
+++ js/3-numbers.js
@@ -0,0 +1,21 @@
+/* Math */
+const numero = 10.5;
+
+/*
+Ceil - Redonde para arriba el decimal
+Floor - Devuelve el entero
+Round - Redondea segun el decimal al entero que sigue o devuelve el actual 
+*/
+
+console.log(Math.ceil(numero));
+console.log(Math.floor(numero));
+console.log(Math.round(numero));
+
+/* min - max */
+console.log(Math.min(9, 8, 1, 5, 6));
+console.log(Math.max(9, 8, 1, 5, 6));
+
+/* random */
+console.log(Math.random());
+/* random entero */
+console.log(Math.round(Math.random() * 25));
---
+++ js/4-condicionales.js
@@ -0,0 +1,40 @@
+/* Condiciones */
+/* Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+ */
+
+/* '16' == 16 - solo el valor */
+/* '16' === 16 - es el valor + el tipo de dato */
+
+/* let edad = prompt("Ingresa tu edad");
+if (Number(edad)) {
+  if (edad >= 18 && edad <= 75) {
+    alert("Puedes tener tu licencia");
+  } else if (Number(edad) === 16) {
+    alert("Podria tener licencia con la autorizacion de los padres.");
+  } else {
+    alert("No puedes tener tu licencia");
+  }
+} else {
+  alert("Ingresaste un dato incorrecto");
+} */
+
+/* Valores de tipo false
+  0 - "" - false - undefined  - null
+  */
+/* 
+let nombre = "a";
+let apellido = "";
+
+if (!nombre) {
+  alert("campo vacio");
+} */
+/* Ternario
+? - es el if
+: - es el else
+*/
+
+/* let nombre = "";
+
+!nombre ? console.log("campo vacio") : console.log("tiene algo");
+
+!nombre && console.log("campo vacio"); */
